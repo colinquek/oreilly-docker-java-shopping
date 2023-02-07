9b429177fe382e651efbdf787265f8585e23d6fe
Colin Quek (NCS) <colinquek@ncs.com.sg>
2023-02-07 10:39:59 +0800

    changed build sh

--- build_all.sh
+++ build_all.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+export MAVEN_OPTS="-Dstyle.color=always -Dhttps.protocols=TLSv1.2 -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=WARN -Dorg.slf4j.simpleLogger.showDateTime=true -DskipTests -Dmaven.test.skip=true -Djava.awt.headless=true -Dproject.build.sourceEncoding=UTF-8 -Dproject.reporting.outputEncoding=UTF-8 -Dmaven.wagon.http.ssl.insecure=true"
+
 cd shopfront
-mvn clean install
+mvn clean package
 cd ..
-cd productcatalogue
-mvn clean install
-cd ..
-cd stockmanager
-mvn clean install
-cd ..
+# cd productcatalogue
+# mvn clean package
+# cd ..
+# cd stockmanager
+# mvn clean package
+# cd ..
 
--- build_all_and_publish_dockerhub.sh
+++ build_all_and_publish_dockerhub.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+export MAVEN_OPTS="-Dstyle.color=always -Dhttps.protocols=TLSv1.2 -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=WARN -Dorg.slf4j.simpleLogger.showDateTime=true -DskipTests -Dmaven.test.skip=true -Djava.awt.headless=true -Dproject.build.sourceEncoding=UTF-8 -Dproject.reporting.outputEncoding=UTF-8 -Dmaven.wagon.http.ssl.insecure=true"
+
 [ -z "$1" ] && echo "Please enter your DockerHub username, example: sh build_all_and_publish_dockerhub.sh username" && exit 1
 echo "dockerhub\`s username: $1"
 
